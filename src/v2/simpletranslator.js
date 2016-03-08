9de854152db3d9ee051d27faa49a8b3a333239fb
Kyungjae Park <kyungjae.park@gmail.com>
2016-03-08 15:34:39 +0900

    번역 추가

--- src/v2/simpletranslator.js
+++ src/v2/simpletranslator.js
@@ -42,6 +42,7 @@ SimpleTranslator = {
             $("#lblSbOptAtt").text("첨부 사진 포함");
             $("#lblSbOptPostLikes").text("게시물 좋아요 여부(PostLike) 포함");
             $("#lblSbOptCommentLink").text("댓글 링크 보기");
+            $("#lblSkipError").text("에러 발생 시 해당 댓글/좋아요 제외");
             $("#lblSbNoteId").text("결과에 표시되는 유저 ID는, 실제 유저 ID가 아닌 App-scoped ID입니다. 다음 설명을 참조하세요 : ");
             $("#lblSbNoteLink").text("[링크]");
             $("#btnExportResultTable").text("결과를 엑셀 파일로 받기 (크롬 전용)");
